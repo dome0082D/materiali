4902f18976245a88cce2a8b122be049dafc05a7c
dome0082D <dome0082@gmail.com>
2026-04-18 10:00:10 +0200

    attivazione tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
 import './globals.css'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="it">
-      <body className="bg-slate-100 text-slate-900">{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
